3f21c7b625df9dfc911e53b5cb1c1117178269f2
Joanna Gajda <gajdula3@gmail.com>
2017-02-16 04:14:39 +0100

    remove unused code

--- lab5/throughput.py
+++ lab5/throughput.py
@@ -8,39 +8,6 @@ from os.path import getsize
 from utilities import read_json
 
 
-def measure_throughput_from_e2(conf, key, key_str, filename):
-    print 'Measure throughput from ec2 instance.'
-    config_path = '../aws/config.json'
-    conf = read_json(config_path)
-    conn = connect_to_region(conf['region'],
-                             aws_access_key_id=conf['aws_access_key'],
-                             aws_secret_access_key=conf['aws_secret_access_key'])
-
-    instance = conn.get_all_instances(instance_ids=[conf['instance_id']])[0].instances[0]
-
-    print '| >Starting instance'
-    start = time.time()
-    instance.start()
-
-    print '| > Waiting for instance'
-    while instance.state != 'running':
-        time.sleep(1.0)
-        instance.update()
-
-    print '| > rCreating ssh client'
-    ssh_client = sshclient_from_instance(instance,
-                                         conf['ssh_key'],
-                                         user_name='ubuntu')
-
-    print 'Start apache2'
-    status, stdout, _ = ssh_client.run('sudo service apache2 start')
-    time.sleep(15)
-
-    print instance.public_dns_name
-    url = 'http://%s/index.html' % instance.public_dns_name
-    pass
-
-
 def measure_throughput(key, file_in, file_out):
     print '| > Upload file to s3'
     start = time.time()
@@ -50,7 +17,7 @@ def measure_throughput(key, file_in, file_out):
 
     print '| > Get file from s3'
     start = time.time()
-    data = key.get_contents_to_filename(file_out)
+    key.get_contents_to_filename(file_out)
     download_time = time.time() - start
     print '| > time: %fs' % download_time
 
