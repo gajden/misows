58a72d8756765073d12dc1bc0de20476ad3cb923
Joanna Gajda <gajdula3@gmail.com>
2017-02-20 23:21:18 +0100

    database operations

--- webapp/database/database.py
+++ webapp/database/database.py
@@ -1,4 +1,6 @@
 import dataset
+import psycopg2
+
 from utilities import read_json
 
 
@@ -26,6 +28,79 @@ class Database(object):
             table = self.db[table]
             table.insert(data)
 
-    def query(self):
-        pass
+    def query_users(self):
+        users = []
+        for user in self.db['users'].all():
+            users.append({
+                "id": user['id'],
+                "username": user['username']
+            })
+        return {"results": users}
+
+    def query_products_bought_by_user(self, user_id, limit=50):
+        activities = self.db['activities'].find(user_id=user_id, bought=True, _limit=limit)
+        bought = []
+        for activity in activities:
+            prod = self.db['products'].find_one(id=activity['prod_id'])
+            bought.append({
+                "id": prod['id'],
+                "description": prod['description'],
+                "category": prod['category'],
+                "color": prod['color'],
+                "picture": prod['picture']
+            })
+
+        return {"results": bought}
+
+    def query_products_viewed_by_user(self, user_id, limit=50):
+        activities = self.db['activities'].find(user_id=user_id, bought=False, _limit=limit)
+        bought = []
+        for activity in activities:
+            prod = self.db['products'].find_one(id=activity['prod_id'])
+            bought.append({
+                "id": prod['id'],
+                "description": prod['description'],
+                "category": prod['category'],
+                "color": prod['color'],
+                "picture": prod['picture']
+            })
+
+        return {"results": bought}
+
+    def query_products(self, category=None, color=None, limit=50):
+        products = self.db['products'].find(category=category, color=color, _limit=limit)
+        result = []
+
+        for prod in products:
+            result.append({
+                "id": prod['id'],
+                "description": prod['description'],
+                "category": prod['category'],
+                "color": prod['color'],
+                "picture": prod['picture']
+            })
+
+        return {"results": result}
+
+    def clean_table(self, table):
+        self.db[table].delete()
+
+    def remove_user(self, user_id):
+        self.db['users'].delete(id=user_id)
+
+
+if __name__ == '__main__':
+    db = Database('./config.json')
+    db.connect()
+    # db.insert('users', {"username": "tom"})
+    img = open('../../data/test.jpg', 'rb').read()
+    img = psycopg2.Binary(img)
+    # db.insert('products', {"description": "pretty dress", "category": "dress",
+    #                        "color": "black", "picture": img})
+    print db.query_users()
+    prod = db.query_products(None, None)
+    with open('hello.jpg', 'wb') as f_out:
+        f_out.write(prod)
+    print db.db
+
 
---
+++ webapp/database/fill_data.py
@@ -0,0 +1,45 @@
+import argparse
+import random
+import string
+
+import psycopg2
+from os import listdir
+
+from os.path import join
+
+from webapp.database.database import Database
+
+
+def fill_database(source_dir, products_num=10000):
+    db = Database('./config.json')
+
+    img_files = [join(source_dir, name) for name in listdir(source_dir)]
+    img_data = []
+    for img_file in img_files:
+        with open(img_file, 'rb') as f_in:
+            img_data.append(psycopg2.Binary(f_in.read()))
+
+    categories = ["dress", "shoes", "trousers", "hats"]
+    colors = ["black", "red", "green", "yellow"]
+
+    for i in xrange(products_num):
+        db.insert('products', {
+            "description": random_word(100),
+            "category": random.choice(categories),
+            "color": random.choice(colors),
+            "picture": random.choice(img_data)
+        })
+
+
+def random_word(length):
+    return ''.join(random.choice(string.lowercase) for i in range(length))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    fill_database()
+
+
+if __name__ == '__main__':
+    main()
